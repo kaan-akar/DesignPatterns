6dc03c516fa99466795e55aa0b3c8ac290f0ae75
KAAN AKAR <kaan.akar@std.yildiz.edu.tr>
2023-07-05 14:11:50 +0300

    composite

---
+++ Composite/Component.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    abstract class Component
+    {
+        public Component() { }
+
+        public abstract string Operation();
+
+        public virtual void Add(Component component) 
+        {
+        
+        }
+
+        public virtual void Remove(Component component) 
+        {
+            
+        }
+
+        public virtual bool IsComposite()
+        {
+            return true;
+        }
+    }
+}
---
+++ Composite/Leaf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    internal class Leaf : Component
+    {
+        public override string Operation()
+        {
+            return "Leaf";
+        }
+
+        public override bool IsComposite()
+        {
+            return false;
+        }
+
+    }
+}
